4d83e7125351c6a8098f47f56a840cafe1835eba
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2023-10-11 11:28:45 -0400

    feat: add config package

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://Cristian-Barajas09.github.io/curso-react-intro-platzi'
 })
